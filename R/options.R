5b0e0b21497b834533d4ef646117a7a66ee433d3
Anish Shah <mrshahman@gmail.com>
2020-12-21 17:20:01 -0600

    updated with octomod, seems to be working

--- R/options.R
+++ R/options.R
@@ -1,22 +1,3 @@
-# Load all your packages before calling make().
-
-# Setup / basics
-library(tidyverse)
-library(tidymodels)
-library(gt)
-
-# Statistical tools
-library(Hmisc)
-library(lme4)
-
-# Tidying 
-library(magrittr)
-library(janitor)
-library(readxl)
-
-# Personal
-library(card)
-
 # Conflicts
 library(conflicted)
 conflict_prefer("select", "dplyr")
